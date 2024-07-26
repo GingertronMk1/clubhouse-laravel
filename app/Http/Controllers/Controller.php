250d4035f67eade8aa0ea0bcaebf195bbfed24e0
Jack Ellis <jackellis1504@gmail.com>
2024-07-26 21:09:39 +0100

    Add inertia method to controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\Support\Arrayable;
+use Inertia\Inertia;
+
 abstract class Controller
 {
-    //
+    protected function inertia(
+        string $component,
+        array|Arrayable $props = []
+    ): \Inertia\Response
+    {
+        return Inertia::render($component, $props);
+    }
 }
