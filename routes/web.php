4b9870827a799423dd59af01c88a605451e7d32b
Jack Ellis <jackellis1504@gmail.com>
2024-07-29 15:42:39 +0100

    wip
     M bin/util/install
     M config/fortify.php
     M package-lock.json
     M resources/js/Layouts/AppLayout.vue
     M resources/js/Pages/Dashboard.vue
     D resources/js/Pages/Welcome.vue
     M routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -4,23 +4,14 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
+    Route::get('/', function () {
         return Inertia::render('Dashboard');
-    })->name('dashboard');
+    })->name('index');
     Route::resources([
         'person' => \App\Http\Controllers\PersonController::class,
     ]);
