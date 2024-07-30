1da7dac9b85e5f8ca7191591ce111aee5c7e6d41
Jack Ellis <jackellis1504@gmail.com>
2024-07-30 08:24:05 +0100

    wip
     M database/seeders/DatabaseSeeder.php
     M resources/js/ssr.js
     M tests/Feature/BuiltIn/ApiTokenPermissionsTest.php
     M tests/Feature/BuiltIn/CreateApiTokenTest.php
     M tests/Feature/BuiltIn/DeleteApiTokenTest.php
     M tests/Feature/BuiltIn/EmailVerificationTest.php
     M tests/Feature/BuiltIn/PasswordConfirmationTest.php

--- resources/js/ssr.js
+++ resources/js/ssr.js
@@ -1,18 +1,21 @@
-import { createSSRApp, h } from 'vue';
-import { renderToString } from '@vue/server-renderer';
-import { createInertiaApp } from '@inertiajs/vue3';
-import createServer from '@inertiajs/vue3/server';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import { createSSRApp, h } from "vue";
+import { ZiggyVue } from "../../vendor/tightenco/ziggy";
+import { createInertiaApp } from "@inertiajs/vue3";
+import createServer from "@inertiajs/vue3/server";
+import { renderToString } from "@vue/server-renderer";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
 createServer((page) =>
     createInertiaApp({
         page,
         render: renderToString,
-        title: (title) => `${title} - ${appName}`,
-        resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+        resolve: (name) =>
+            resolvePageComponent(
+                `./Pages/${name}.vue`,
+                import.meta.glob("./Pages/**/*.vue"),
+            ),
         setup({ App, props, plugin }) {
             return createSSRApp({ render: () => h(App, props) })
                 .use(plugin)
@@ -21,5 +24,6 @@ createServer((page) =>
                     location: new URL(page.props.ziggy.location),
                 });
         },
-    })
+        title: (title) => `${title} - ${appName}`,
+    }),
 );
