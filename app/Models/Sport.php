da2c30dcfde2eeb94da788ab31e12bfe1a6df0c1
Jack Ellis <jackellis1504@gmail.com>
2024-08-02 09:47:20 +0100

    fix relationships
     M app/Models/Sport.php

--- app/Models/Sport.php
+++ app/Models/Sport.php
@@ -6,6 +6,7 @@ use Database\Factories\SportFactory;
 use Illuminate\Database\Eloquent\Concerns\HasVersion7Uuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Sport extends Model
@@ -23,4 +24,20 @@ class Sport extends Model
     ];
 
     protected $casts = [];
+
+    /**
+     * @return HasMany<Team>
+     */
+    public function teams(): HasMany
+    {
+        return $this->hasMany(Team::class);
+    }
+
+    /**
+     * @return HasMany<Position>
+     */
+    public function positions(): HasMany
+    {
+        return $this->hasMany(Position::class);
+    }
 }
