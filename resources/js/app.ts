14e7fd4cded447172af83c7a12b6630a1c202247
Jack Ellis <jackellis1504@gmail.com>
2024-08-04 14:12:06 +0100

    so i just made a new laravel project with typescript and copied stuff over lol
     M package.json
     M resources/js/app.ts
     M resources/js/bootstrap.ts
     M resources/js/ssr.ts
    A  resources/js/types/global.d.ts
    A  resources/js/types/index.d.ts
    A  resources/js/types/vite-env.d.ts
     M tsconfig.json

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,52 +1,23 @@
-import "./bootstrap";
-import "../css/app.css";
-import "../scss/app.scss";
+import './bootstrap';
+import '../css/app.css';
 
-import type { DefineComponent } from "vue";
-import type {InertiaApp, InertiaAppProps} from "@inertiajs/vue3/types/app";
-import type { PluginOption } from "vite";
+import { createApp, h, DefineComponent } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
-import {
-    createApp,
-    h
-} from "vue";
-import { ZiggyVue } from "../../vendor/tightenco/ziggy";
-import { createInertiaApp } from "@inertiajs/vue3";
-import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
-
-const appName = import.meta.env.VITE_APP_NAME || "Laravel";
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    progress: {
-        color: "#4B5563",
-    },
-    resolve: (name: string) =>
-        resolvePageComponent(
-            `./Pages/${name}.vue`,
-            import.meta.glob<DefineComponent>("./Pages/**/*.vue"),
-        ),
-    setup: function (
-        {
-            el,
-            App,
-            props,
-            plugin
-        }:
-            {
-                el: Element,
-                App: InertiaApp,
-                props: InertiaAppProps,
-                plugin: PluginOption
-            }) {
-        return createApp(
-            {render: () => h(App, props)})
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
             .mount(el);
     },
-    title: (title: string) => (
-        title.length
-            ? `${title} - ${appName}`
-            : appName
-    ),
+    progress: {
+        color: '#4B5563',
+    },
 });
--- resources/js/ssr.ts
+++ resources/js/ssr.ts
@@ -1,21 +1,18 @@
-import { createSSRApp, h } from "vue";
-import { ZiggyVue } from "../../vendor/tightenco/ziggy";
-import { createInertiaApp } from "@inertiajs/vue3";
-import createServer from "@inertiajs/vue3/server";
-import { renderToString } from "@vue/server-renderer";
-import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { createSSRApp, h, DefineComponent } from 'vue';
+import { renderToString } from '@vue/server-renderer';
+import { createInertiaApp } from '@inertiajs/vue3';
+import createServer from '@inertiajs/vue3/server';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
-const appName = import.meta.env.VITE_APP_NAME || "Laravel";
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createServer((page) =>
     createInertiaApp({
         page,
         render: renderToString,
-        resolve: (name) =>
-            resolvePageComponent(
-                `./Pages/${name}.vue`,
-                import.meta.glob("./Pages/**/*.vue"),
-            ),
+        title: (title) => `${title} - ${appName}`,
+        resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
         setup({ App, props, plugin }) {
             return createSSRApp({ render: () => h(App, props) })
                 .use(plugin)
@@ -24,6 +21,5 @@ createServer((page) =>
                     location: new URL(page.props.ziggy.location),
                 });
         },
-        title: (title) => `${title} - ${appName}`,
-    }),
+    })
 );
