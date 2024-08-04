c5bf237b1c9e13e97e7bceb583b5d3eaed958611
Jack Ellis <jackellis1504@gmail.com>
2024-08-04 15:28:53 +0100

    add types
     M resources/js/types/global.d.ts

--- resources/js/types/global.d.ts
+++ resources/js/types/global.d.ts
@@ -19,3 +19,49 @@ declare module "vue" {
 declare module "@inertiajs/core" {
     interface PageProps extends InertiaPageProps, AppPageProps {}
 }
+
+interface User {
+    id: string,
+    name: string,
+    email: string,
+}
+
+interface Person {
+    id: string,
+    name: string,
+    bio: ?string,
+    dob: ?string,
+    user_id: ?string,
+    user: ?User
+}
+
+interface Team {
+    id: string,
+    name: string,
+    description: ?string,
+    address: ?string,
+    sport_id: string,
+    sport: Sport,
+    people: ?Array<Person>
+}
+
+interface Sport {
+    id: string,
+    name: string,
+    description: ?string,
+    colour: ?string,
+    teams: ?Array<Team>
+    positions: ?Array<Position>,
+}
+
+interface Position {
+    id: string,
+    name: string,
+    description: ?string,
+    sort_order: bigint,
+    default_number: bigint,
+    sport_id: string,
+    sport: Sport,
+    preview_position_x: bigint,
+    preview_position_y: bigint,
+}
