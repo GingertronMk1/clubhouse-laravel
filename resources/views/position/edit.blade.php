d416caf879887c86934efe0373395922f2d65ce4
Jack Ellis <jackellis1504@gmail.com>
2024-08-16 15:36:44 +0100

    Competition form

--- resources/views/position/edit.blade.php
+++ resources/views/position/edit.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <form action="{{ route('position.update') }}" method="post">
+    <form action="{{ route('position.update', ['position' => $position]) }}" method="post">
         @csrf
         @method('PUT')
         <x-input name="name" :value="$position->name" />
@@ -8,7 +8,7 @@
         <x-input name="default_order" type="number" :value="$position->default_number" />
         <x-input name="preview_x" type="range" value="0" min="0" max="100" :value="$position->preview_x" />
         <x-input name="preview_y" type="range" value="0" min="0" max="100" :value="$position->preview_y" />
-        <x-input name="sport_id" type="select" :options="\App\Models\Sport::all()" />
+        <x-input name="sport_id" type="select" :options="\App\Models\Sport::all()" :value="$position->sport_id" />
         <button type="submit">Update</button>
     </form>
 
