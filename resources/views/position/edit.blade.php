c8eb35501cfc78e4d02cf9d466c7f932e0a99c58
Jack Ellis <jackellis1504@gmail.com>
2024-08-16 15:34:36 +0100

    Competition's sport is not nullable

--- resources/views/position/edit.blade.php
+++ resources/views/position/edit.blade.php
@@ -1,3 +1,15 @@
 <x-app-layout>
+    <form action="{{ route('position.update') }}" method="post">
+        @csrf
+        @method('PUT')
+        <x-input name="name" :value="$position->name" />
+        <x-input name="description" type="textarea" :value="$position->description" />
+        <x-input name="sort_order" type="number" :value="$position->sort_order" />
+        <x-input name="default_order" type="number" :value="$position->default_number" />
+        <x-input name="preview_x" type="range" value="0" min="0" max="100" :value="$position->preview_x" />
+        <x-input name="preview_y" type="range" value="0" min="0" max="100" :value="$position->preview_y" />
+        <x-input name="sport_id" type="select" :options="\App\Models\Sport::all()" />
+        <button type="submit">Update</button>
+    </form>
 
 </x-app-layout>
